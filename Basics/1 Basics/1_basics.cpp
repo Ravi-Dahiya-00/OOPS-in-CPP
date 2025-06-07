d27cf264c4925eeb2868c24667ad6b6a9b4a8cd0
RAVI-DAHIYA--00 <ravidahiya9350@gmail.com>
2025-06-07 08:59:38 +0530

    started Object oriented Programming in C++

---
+++ Basics/1 Basics/1_basics.cpp
@@ -0,0 +1,174 @@
+// Standard IO header is iostream and uses cin and cout are used for input/output in C++.
+// File extension is ".cpp" or ".c++" or ".cc" or ".cxx"
+
+
+
+// Necessary header files for input output functions
+
+#include <iostream>
+
+// Preprocessor Directives:
+        /*The #include is a preprocessor directive that tells the compiler to include the content of a file in the source code. 
+        For example, #include<iostream> tells the compiler to include the input-output library which contains all C++'s input/output
+         library functions.
+           */
+
+
+
+// Header Files : 
+        /*In C++ language header file is a file that contains features like functions, data types, macros, etc that can be used by
+         any other C program by including that particular header file using “#include” preprocessor. Header files generally ends with the .h
+          extension although in modern C++ many header files do not have the .h extension
+            */
+// header file without .h extension
+#include <iostream> 
+// header file with .h extension
+#include<string.h>
+
+
+
+using namespace std;       // Allows using cout instead of std::cout
+// A namespace groups related code to avoid name conflicts; 'std' holds C++ built-in features like cout, cin, etc.
+
+
+// main() function: where the execution of C++ program begins
+
+    /*
+    The main() function is the entry point of every C++ program when the program is executed the code written in the main functions
+    is executed first. The opening braces '{' indicates the beginning of the main function and the closing braces '}' indicates 
+    the ending of the main function.
+    */
+
+int main()
+{
+    // This statement prints "Hello World"
+    cout<<"Hello World\n";
+
+    cout << "Learning C++ at GeekforGeeks" << endl;
+    // cout: Stands for "character output", and is used to print output to the console. It comes from the std namespace.
+    // endl: Stands for "end line", which moves the cursor to the next line, just like \n.
+    // <<: This is the insertion operator, which sends data to the output stream (in this case, to cout).
+
+
+
+
+    // std: Refers to the standard namespace where built-in C++ features like cout, cin, endl are defined.
+    // Prints "hello guys" using standard namespace without 'using namespace std'
+    std::cout<< "hello guys";
+    
+    /*
+    The cout is a output statement in C++ that is used to display output on the console screen. 
+    Everything followed by the character << in double quotes " " is displayed on the output screen. 
+    The semi-colon character at the end of the statement is used to indicate that the statement is ending there.
+        */
+
+    return 0;
+
+    /*
+        The return statement is used to return a value from a function and indicates the finishing of a function.
+        Here, it is used to sent the signal of successful execution of the main function.
+            */
+}
+
+
+
+
+// comments : 
+        // Comments explain code; skipped by compiler, not part of execution.
+
+                //  This is a single line comment
+
+                /* This is a multi-line comment
+                which can span multiple lines */
+
+
+
+
+
+
+
+
+
+
+
+/*
+Features of C++ :
+    1.Simple
+    2.Machine Independent
+    3.Low-level Access
+    4.Fast Execution Speed
+    5.Object-Oriented
+    6.Dynamic Memory Allocation
+    7.Multi-threading
+    8.Memory Management
+
+
+
+
+
+
+
+** C++ is used in a wide range of applications from game engines and application software to operating systems and embedded systems.
+
+** It supports both low-level and high-level features such as manual memory management and OOPs programming respectively.
+
+** C++ is known as hybrid language because C++ supports both procedural and object-oriented programming paradigms.
+
+
+✅ Procedural Programming Language:
+        A procedural programming language is a type of programming language where the code is organized into procedures or functions.
+           These procedures contain a sequence of instructions that the computer follows step by step.
+
+        example: c,go,pascal,basic,FORTRAN
+
+            ✅ Advantages:
+                Simple and easy to understand.
+
+                Good for small to medium-sized programs.
+
+                Reusable code using functions.
+
+            ❌ Disadvantages:
+                Harder to manage in large, complex systems.
+
+                Not good at modeling real-world entities (unlike OOP).
+
+                Poor data security due to global variables.
+
+
+
+
+
+
+
+✅ Object-Oriented Programming (OOP) Language :
+        An Object-Oriented Programming (OOP) language is a type of programming language that organizes code using objects,
+         which are instances of classes. It focuses on real-world modeling, combining data (attributes) and functions (methods) 
+         that operate on that data into a single unit — the object.
+
+         example:c++,java,python,c#,Ruby
+
+         Think of a class as a blueprint (like a car design), and an object as the actual car built using that blueprint.
+
+
+
+
+
+            ✅ Advantages:
+                Better code organization
+
+                Reusability through inheritance
+
+                Data security via encapsulation
+
+                Easier to manage large projects
+
+            ❌ Disadvantages:
+                Can be more complex to learn initially
+
+                Overhead in performance compared to procedural in some cases
+
+
+                
+
+*/
---
+++ Basics/2 variables and constraints/1_variables.c++
@@ -0,0 +1,149 @@
+#include <iostream>
+using namespace std;
+#define PI 3.14
+
+/* 
+    #define is a preprocessor directive in C++ used to define symbolic (macro) constants.
+    It tells the compiler to replace every occurrence of a name with a value before compilation starts.   
+    
+    
+    Text replacement:	The preprocessor replaces PI with 3.14 before compilation.
+    No data type:	It does not have a type (unlike const), so there's no type safety.
+    Global scope:	Available throughout the file after it’s defined.
+    Cannot be debugged:     Since it’s replaced before compiling, debuggers cannot track it.
+
+	
+
+*/
+
+
+int main()
+{
+
+    
+    /*
+    In C++, variable is a name given to a memory location. It is the basic unit of storage in a program.
+     The value stored in a variable can be accessed or changed during program execution.
+
+     type name;
+     type name1, name2, name3 ....;
+
+            type is the type of data that a variable can store, and name is the name assigned to the variable.
+    */
+
+
+    int num;  //To store number without decimal point, we use integer data type.
+    // int is the keyword used to tell the compiler that the variable with name num will store integer values
+
+    // Initializing : A variable that is just defined may not contain some valid value. We have to initialize it to some valid initial value.
+    num = 3;
+
+
+    // Definition and initialization can also be done in a single step as shown:
+    int num111 = 3;
+
+
+
+    // Accessing and printing above variable
+    cout << num << endl;
+
+
+    // Note: The value we assign should be of the same type as the variable.
+
+
+    // Accessing and Updating
+    /*
+    The main objective of a variable is to store the data so that it can be retrieved or update any time.
+     Accessing can be done by simply using its assigned name and updating the value using = assignment operator.
+        */
+
+
+
+    // Update the value
+    num = 7;
+    
+    cout << num << endl;
+
+
+
+
+// Rules For Naming Variable
+     // The names given to a variable are called identifiers. There are some rules for creating these identifiers (names):
+          /*
+        1.A name can only contain letters (A-Z or a-z), digits (0-9), and underscores (_).
+        2.It should start with a letter or an underscore only.
+        3.It is case sensitive.
+        4.The name of the variable should not contain any whitespace and special characters (i.e. #, $, %, *, etc).
+        5.We cannot used C++ keyword (e.g. float, double, class) as a variable name.
+            */
+
+
+        
+        int num1 = 10, num2;
+    
+    // Assigning num1's value to num2
+    num2 = num1;
+    cout << num1 << " " << num2 << endl;
+
+
+// Addition of two integers can be done in C++ using + operator as shown:
+    cout << 10 + 20 << endl;
+
+// We can do the above operation using the variables that store these two values.
+    cout << num1 + num2 << endl;
+
+
+
+
+    // Constant Variables:
+        // In C++, a constant variable is one whose value cannot be changed after it is initialized. This is done using the const keyword.
+
+
+
+
+    const int new_num=20;
+
+    cout << new_num << endl;
+
+
+
+
+
+    cout << PI << endl;
+
+
+
+    float radius = 5;
+    float area = PI * radius * radius;
+    std::cout << "Area: " << area << std::endl;
+
+
+
+    /*
+    1. const is type-safe and respects scope rules.
+    2. You can use const in debugging and type-checking.
+    3. Prefer const in modern C++ unless you need macros for specific cases.
+      */
+
+    // Scope of Variables 
+        /*
+        Scope of variable is the region inside the program where the variable can be referred to by using its name. 
+        Basically, it is the part of the program where the variable exists. Proper understanding of this concept 
+        requires the understanding of other concepts such as functions, blocks, etc.
+             */
+
+
+
+    // Memory Management of Variables
+    /*
+    When we create or declare a variable, a fixed-size memory block is assigned to the variable, 
+    and its initial value is a garbage value. Initialization assigns a meaningful value using the assignment operator. 
+    Variables essentially manipulate specific memory locations, and their stored data is accessed via their names.
+        */
+
+
+
+
+    return 0;
+
+}
